043a6c76cdc42ae07e8b58ad94b57c53ea749b11
ismail <ismail.hussainx0@gmail.com>
2026-06-28 01:38:46 +0500

    just create seller profile

--- services.js
+++ services.js
@@ -4,21 +4,60 @@ let searchInput = document.querySelector("#searchInput");
 // card
 let servicesCard = document.querySelectorAll(".service-card")
 
-searchInput.addEventListener("input", function(){
+searchInput.addEventListener("input", function () {
 
-    let searchValue = searchInput.value.toLowerCase();
+     // search value to lower case
+     let searchValue = searchInput.value.toLowerCase();
 
-    servicesCard.forEach(function(item){
-       let cardTitle =  item.querySelector(".service-title").innerText.toLowerCase();
+     servicesCard.forEach(function (item) {
+          let cardTitle = item.querySelector(".service-title").innerText.toLowerCase();
 
-       if(cardTitle.includes(searchValue)){
-            item.parentElement.style.display = "";  
-       }else{
-            item.parentElement.style.display = "none";
-       }
+          if (cardTitle.includes(searchValue)) {
+               item.parentElement.style.display = "";
+          } else {
+               item.parentElement.style.display = "none";
+          }
 
-    })
-
-
+     })
 
 })
+
+
+
+// Search Filter
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function (btn) {
+
+
+     btn.addEventListener("click", function () {
+
+          // button colors change
+          buttons.forEach(function (button) {
+               button.classList.remove("primary-btn");
+               button.classList.add("secondary-btn");
+          })
+
+          btn.classList.remove("secondary-btn");
+          btn.classList.add("primary-btn");
+
+
+
+          // btn data category
+          let selectedValue = btn.dataset.category
+
+          // card loop
+          servicesCard.forEach(function (card) {
+               let value = card.dataset.category;
+
+               if (selectedValue === value || selectedValue === "all") {
+                    card.parentElement.style.display = "";
+               } else {
+                    card.parentElement.style.display = "none"
+               }
+          })
+
+     })
+
+})
+
